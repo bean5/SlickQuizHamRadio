bb1cc1c60b06a3df4858c2334ced7f91e52bb7c0
Michael Bean <michael.bean@byu.edu>
2015-07-13 09:57:47 -0600

    Allow user to set some options before trying

--- js/master.js
+++ js/master.js
@@ -1,5 +1,31 @@
 // Put all your page JS here
 
 $(function () {
-    $('#slickQuiz').slickQuiz();
+    //Default way of setting up upon page load
+    // $('#slickQuiz').slickQuiz(
+    //   {
+    //     skipStartButton: true,
+    //     randomSortQuestions: true
+    //   }
+    // );
+
+    //setting things up when options are set for quiz.
+    $('#random').on('click', function() {
+      setup(true);
+    })
+
+    $('#notRandom').on('click', function() {
+      setup(false);
+    })
+
+    function setup(randomSortQuestions) {
+      $('#slickQuiz').slickQuiz(
+        {
+          skipStartButton: true,
+          randomSortQuestions: randomSortQuestions
+        }
+      );
+
+      $('#options').remove();
+    }
 });
